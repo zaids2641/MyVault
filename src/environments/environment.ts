acd7d45e27a1c51a07e6a118b873ef9ff132f8ae
zaids2641 <39379399+zaids2641@users.noreply.github.com>
2022-07-31 15:21:13 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebase: {
-    projectId: 'myvault-1c15e',
-    appId: '1:617764401217:web:210cd9560a959f026c56c0',
-    storageBucket: 'myvault-1c15e.appspot.com',
-    apiKey: 'AIzaSyD9yw9TQnjkfA6lg7lcXkbntIKy0b1oOYM',
-    authDomain: 'myvault-1c15e.firebaseapp.com',
-    messagingSenderId: '617764401217',
+    projectId: 'xxxxxxxxxxxxx',
+    appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    storageBucket: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    messagingSenderId: 'xxxxxxxxxxxx',
   },
   production: false
 };
